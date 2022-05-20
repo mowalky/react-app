cd57ef9fec781e638f2067a870a8302b95aa6fef
Mowalky <dev@mowalky.com>
2022-05-20 13:43:33 -0500

    scoped css

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 
+import classes from "./MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
-      <div>react Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>react Meetups</div>
       <nav>
         <ul>
           <li>
