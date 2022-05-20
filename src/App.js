407e3c0b0ef51824b0b536991403741bf2d4b4ba
Mowalky <dev@mowalky.com>
2022-05-19 22:56:48 -0500

    setup project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorite";
+import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Exploring React" />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,5 @@
+function AllMeetingupsPage() {
+  return <div>All Meetup Page</div>;
+}
+
+export default AllMeetingupsPage;
