a6d1639de4d7dc9ca16e7a7e1abae810ca9e38d1
Mowalky <dev@mowalky.com>
2022-05-20 17:17:22 -0500

    prep for http request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function onAddMeetupHandler(data) {
+    console.log(data);
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </section>
   );
 }
