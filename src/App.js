722def636e2d95351dbaeab23ee638714390c04c
Mowalky <dev@mowalky.com>
2022-05-20 15:18:10 -0500

    card style added

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorite";
 import NewMeetupPage from "./pages/NewMeetup";
 
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
