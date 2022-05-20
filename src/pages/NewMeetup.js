a5a0a1947f6f2f79434da5ddfe321f734c2aad34
Mowalky <dev@mowalky.com>
2022-05-20 17:22:35 -0500

    redirect on submit

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function onAddMeetupHandler(meetupData) {
     fetch(`https://react-app-3469b-default-rtdb.firebaseio.com/meetups.json`, {
       method: "POST",
@@ -8,7 +12,7 @@ function NewMeetupPage() {
       headers: {
         "Content-Type": "application/json",
       },
-    });
+    }).then(() => history.replace("/"));
   }
 
   return (
