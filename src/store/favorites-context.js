015d9ae428a3193a58799eaf28394afdee25074a
Mowalky <mowalky@outlook.com>
2022-05-21 22:19:00 -0500

    updating context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,9 +1,27 @@
 import { createContext, useState } from "react";
 
-const FavoritesContext = createContext({ favorites: [], totalFavorites: [] });
+const FavoritesContext = createContext({ favorites: [], totalFavorites: 0 });
 
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeeptup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeeptup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter(
+        (favorite) => favorite.meetupId !== meetupId
+      );
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((favorite) => favorite.meetupId === meetupId);
+  }
 
   const context = {
     favorites: userFavorites,
